5df7e6b7f1246454de98a04a9356baac0c65f45c
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-15 16:35:35 +0300

    refactor: change app display

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import Raiting from "./Raiting";
 import Accordion from "./Accordion";
 import {UncontroledOnOff} from "./UncontroledOnOff";
 import {OnOff} from "./components/OnOff/OnOff";
+import { Select } from "./components/Select/Select";
+const dats = [
+    {name: 'user1', value: '1'},
+    {name: 'user2', value: '2'},
+    {name: 'user3', value: '3'}
+]
 
 function App() {
   return (
     <div className="App">
-     <Raiting/>
-        <Accordion />
-        <UncontroledOnOff />
+     {/*<Raiting/>*/}
+     {/*   <Accordion />*/}
+     {/*   <UncontroledOnOff />*/}
         {/*<OnOff/>*/}
+
+        <Select name={'Its select'} list={dats} onChange={(x) => console.log(x) }/>
     </div>
   );
 }
